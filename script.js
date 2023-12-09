d8596ee396f20723d88ef27fec1c218745425019
Othei26 <gutierrezmaiker@gmail.com>
2023-12-09 02:01:01 -0600

    Add interactive UI
    Adapted the JS to interact with the UI

--- script.js
+++ script.js
@@ -2,7 +2,12 @@
 const optionsAvail = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
-const rounds = 5;
+const rounds = 3;
+
+
+// Select all the Elements
+let optionsBtn = document.querySelectorAll(".options_btn");
+
 
 // Function to randomly select computer's choice from the Options Available in the Game.
 function getComputerChoice() {
@@ -10,49 +15,107 @@ function getComputerChoice() {
 }
 
 // Function to play a single round.
-function playRound() {
-
-    const playerSelect = prompt("Type your option:");
-
-    // Check if the user clicked Cancel or entered an empty value before converting the input to lowerCase.
-    if (playerSelect === null || playerSelect === "") {
-        console.log("No option provided or it was canceled");
-        return; // Exit the function if canceled.
-    };
-
-    const playerSelection = playerSelect.toLowerCase().trim();
-    const computerSelection = getComputerChoice();
-
-    if (!optionsAvail.includes(playerSelection)) {
-        console.log("Invalid Option");
-    } else if (playerSelection === computerSelection) {
-        console.log("You tied");
+function playRound(playerSelection) {
     
+    const computerSelection = getComputerChoice();
+    
+    if (playerSelection === computerSelection) {
+        console.log("You Tied");
+        addPlayDetails(playerScore, computerScore, computerSelection, playerSelection);
     } else if (
         playerSelection === "paper" && computerSelection === "rock" ||
         playerSelection === "rock" && computerSelection === "scissors" ||
         playerSelection === "scissors" && computerSelection === "paper"
-    ) {
-        playerScore += 1;
-        console.log("You Win!!");
-    } else {
-        computerScore += 1;
-        console.log(`You Lose, ${computerSelection} beats ${playerSelection}`);
+        ) {
+            playerScore += 1;
+            console.log("You Win!!");
+            addPlayDetails(playerScore, computerScore, computerSelection, playerSelection);
+        } else {
+            computerScore += 1;
+            console.log(`You Lose, ${computerSelection} beats ${playerSelection}`);
+            addPlayDetails(playerScore, computerScore, computerSelection, playerSelection);
+        }
     }
+    
+// Handle button click events
+function handleButtonClick(event) {
+    const playerSelection = event.target.textContent.toLowerCase();
+    playRound(playerSelection);
+ }
+    
+// Add Event Listeners to Buttons
+optionsBtn.forEach((btn) => {
+    btn.addEventListener("click", handleButtonClick);
+})
+
+    
+// ===================================================
+    
+    
+
+
+// Add Scores to the Score Container
+function addScores(playerScore, computerScore) {
+        
+    const scoreSection = document.querySelector("#score");
+        
+    const playerP = document.createElement("p");
+    const computerP = document.createElement("p");
+        
+    playerP.textContent = `Player: ${playerScore} points`;
+    computerP.textContent = `Computer: ${computerScore} points`;
+        
+    scoreSection.appendChild(playerP);
+    scoreSection.appendChild(computerP);
+}
+    
+    
+// Add Oponent's Choice to the Oponent's Choice Container
+function addOponentChoice(computerSelection) {
+    
+    const oponentChoiceSection = document.querySelector("#oponent_choice");
+
+    const oponentChoiceP = document.createElement("p");
+    oponentChoiceP.textContent = computerSelection;
+
+    oponentChoiceSection.appendChild(oponentChoiceP);
 }
 
-// Function to print the final game results
-function printGameResults() {
-    if (playerScore > computerScore) {
-        console.log(`Player Wins with ${playerScore} points!!!`);
-    } else {
-        console.log(`Computer Wins with ${computerScore} points!!!`);
-    }
+
+// Add Match Result to the History Section
+function addMatchHistory(playerSelection, computerSelection) {
+
+    const historySection = document.querySelector("#history");
+
+    const historyP = document.createElement("p");
+    historyP.textContent = `Player: ${playerSelection} Computer: ${computerSelection}`;
+
+    historySection.appendChild(historyP);
 }
 
-// Loop to play the specified number of rounds.
-for (let i = 0; i < rounds; i++) {
-    playRound();
+// Function to play all the functions
+function addPlayDetails(playerScore, computerScore, computerSelection, playerSelection) {
+        addScores(playerScore, computerScore);
+        addOponentChoice(computerSelection);
+        addMatchHistory(playerSelection, computerSelection);
 }
-// Print the final game results.
-printGameResults();
+
+
+// // Function to change the border color of the choices
+// function changeBorderChoice() {
+
+//     const gameOptionsSection = document.querySelectorAll(".game_options");
+
+//     gameOptionsSection.forEach((optSect) => {
+
+//         const borderOptionChange = optSect.target.id;
+    
+
+//         // Modify the border color depending in each participant's choice 
+
+//         borderOptionChange.style.borderStyle = "3px solid red";
+//         borderOptionChange.style.borderStyle = "3px solid green";
+
+
+//     })
+// }
